1578c1d9e32cdfd09477cbfb46aff6872e4c5340
Quentin Cartier <daoliangshumaishuji@gmail.com>
2025-03-11 09:16:08 +0100

    Modify s3-example to list my bucket

--- week3/s3-example/src/main.rs
+++ week3/s3-example/src/main.rs
@@ -4,12 +4,18 @@ use aws_config::{BehaviorVersion, Region};
 #[::tokio::main]
 async fn main() -> Result<(), s3::Error> {
 
-    let config = aws_config::defaults(BehaviorVersion::latest()).region(Region::new("us-east-1")).load().await;
+    // Load configuration from the shared credentials file ~/.aws/credentials
+    // In my case, my bucket is located at eu-norht-1
+    // Be sure to replace "eu-north-1" with your region
+    // Be sure to replace "mofafendata" with your bucket
+    // Be sure to have set proper policies for your user (ListBuckets and ListObjects)
+    let config = aws_config::defaults(BehaviorVersion::latest()).region(Region::new("eu-north-1")).load().await;
 
     let client = aws_sdk_s3::Client::new(&config);
 
     // ... make some calls with the client
-    let res = client.list_buckets().send().await?;
+    //let res = client.list_buckets().send().await?;
+    let res = client.list_objects_v2().bucket("mofafendata").send().await?;
     println!("{:?}", res);
     Ok(())
 }
